638d05f72c15ca26a2716edc93723002ebd501f9
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-08-05 13:56:55 -0400

    Initial Commit

---
+++ Templates_and_Generic_Programming_in_C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Templates_and_Generic_Programming_in_C++
+//
+//  Created by Ferenc Szalma on 8/5/16.
+//  Copyright © 2016 Ferenc Szalma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
